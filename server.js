78ecc5e966b7bfa60f5e787c71a51cff2ad6bc55
yaseminvatan <yvatan035@gmail.com>
2024-09-19 14:49:11 -0700

    ensuring that all routes fall back to serving the main HTML page

--- server.js
+++ server.js
@@ -16,3 +16,7 @@ app.use(express.static('public'));
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
