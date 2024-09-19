670ce46dd003bc39135f921a0d23033e6c36996e
yaseminvatan <yvatan035@gmail.com>
2024-09-19 11:57:17 -0700

    adds a middleware that parses incoming requests with JSON payloads and  middleware parses incoming requests with URL-encoded payloads

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const { v4: uuidv4 } = require('uuid'); // for each note a uniqie id
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
